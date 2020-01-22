1d814edc2f6875bcdd7c5e46f476946063ccacc5
DBHeise <david@heiseink.com>
2020-01-22 10:44:27 -0500

    Fixing RemoteAddresses (with tests)

---
+++ RoboDave.Tests/Network/RemoteAddressesCmdletTest.cs
@@ -0,0 +1,31 @@
+﻿
+namespace RoboDave.Tests.Network
+{
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RoboDave.Generators;
+    using RoboDave.Network;
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Management.Automation.Runspaces;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class RemoteAddressesCmdletTest : RoboDave.Tests.PSCmdletTest
+    {
+        [TestMethod]
+        public void BasicAlgoVerify()
+        {
+            ps.AddCommand("Get-RemoteAddresses");              
+
+            var list = this.DoInvoke<RemoteAddressInfo>();
+            Assert.AreEqual(6, list.Count);
+        }
+    }
+}
--- RoboDave/Network/RemoteAddresses.cs
+++ RoboDave/Network/RemoteAddresses.cs
@@ -8,32 +8,160 @@ namespace RoboDave.Network
     using System.Net;
     using System.Net.NetworkInformation;
     using System.Net.Sockets;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public struct RemoteAddressInfo
+    {
+        public String Provider { get; private set; }
+        public String Status { get; private set; }
+        public IPAddress Address { get; private set; }
+        public String Raw { get; private set; }
+
+        public RemoteAddressInfo(String provider, String status, IPAddress address)
+        {
+            this.Provider = provider;
+            this.Status = status;
+            this.Address = address;
+            this.Raw = null;
+        }
+        public RemoteAddressInfo(String provider, String status, String address)
+        {
+            this.Provider = provider;
+            this.Status = status;
+            this.Raw = address;
+            this.Address = null;
+            IPAddress a;
+            if (status == "Success")
+            {
+                if (IPAddress.TryParse(address.Trim(), out a))
+                {
+                    this.Address = a;
+                }
+                else
+                {
+                    var match = RemoteAddressesCmdlet.ipv4Regex.Match(address);
+                    if (match != null && match.Success)
+                    {
+                        this.Address = IPAddress.Parse(match.Value);
+                    }
+                    else
+                    {
+                        this.Status = "Invalid IPAddress";
+                    }
+                }
+            }
+
+        }
+    }
 
 
     [Cmdlet(VerbsCommon.Get, "RemoteAddresses", SupportsShouldProcess = true)]
-    [OutputType(typeof(IPAddress))]
+    [OutputType(typeof(RemoteAddressInfo))]
     public class RemoteAddressesCmdlet : PSCmdlet
     {
-
-        private String[] remoteServers;
+        internal static Regex ipv4Regex = new Regex(@"(?:(?:\d|[01]?\d\d|2[0-4]\d|25[0-5])\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d|\d)(?:\/\d{1,2})?");
 
         public RemoteAddressesCmdlet()
         {
-            this.remoteServers = new string[] {"http://icanhazip.com","http://freegeoip.net/xml/","http://www.whatismyip.com/","http://api.hostip.info/get_html.php","http://myip.dnsdynamic.org","http://checkip.dyndns.com"};
+            this.RemoteEndpoints = new string[] {
+                "http://icanhazip.com",
+                "http://www.whatismyip.com/",
+                "http://api.hostip.info/get_html.php",
+                "http://checkip.dyndns.com",
+                "https://myexternalip.com/raw",
+                "dns://resolver1.opendns.com/myip.opendns.com"
+            };
         }
+
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "List of remote endpoints")]
+        public String[] RemoteEndpoints { get; set; }
 
 
         protected override void ProcessRecord()
         {
-
-            Uri remoteServer = new Uri(Rando.RandomPick(this.remoteServers));
-            WriteVerbose("Using Remote Service: " + remoteServer.ToString());
-            String address;
-            using (WebClient client = new WebClient())
+            foreach (var server in this.RemoteEndpoints)
             {
-                address = client.DownloadString(remoteServer).Trim();
+                Uri u = new Uri(server);
+
+                WriteVerbose("Using Remote Endpoint: " + u.ToString());
+
+                String response = null;
+                String status = null;
+                String provider = u.Host;
+
+                if (u.Scheme == "http" || u.Scheme == "https")
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        try
+                        {
+                            response = client.DownloadString(u);
+                            status = "Success";
+                        }
+                        catch (WebException we)
+                        {
+                            status = "Exception: " + we.Status;
+                            if (we.InnerException != null)
+                            {
+                                if (we.InnerException is System.Net.Sockets.SocketException)
+                                {
+                                    status = "SocketException: " + we.InnerException.Message;
+                                }
+                                else
+                                {
+                                    status = we.InnerException.GetType().Name + ": " + we.InnerException.Message;
+                                }
+                            }
+                            else
+                            {
+                                var webResponse = we.Response;
+                                if (webResponse != null)
+                                {
+                                    status = "HttpResponse: " + ((HttpWebResponse)webResponse).StatusCode.ToString();
+                                    Stream stream = webResponse.GetResponseStream();
+                                    if (stream != null)
+                                    {
+                                        StreamReader reader = new StreamReader(stream, true);
+                                        response = reader.ReadToEnd();
+                                        reader.Dispose();
+                                        stream.Dispose();
+                                    }
+                                    webResponse.Dispose();
+                                }
+
+                            }
+                        }
+                    }
+                }
+                else if (u.Scheme == "dns")
+                {                    
+                    String dnsserver = u.Host;
+                    String lookup = u.LocalPath.Trim('/');
+                 
+                    var proc = new System.Diagnostics.Process();
+                    proc.StartInfo = new System.Diagnostics.ProcessStartInfo("nslookup.exe", lookup + " " + dnsserver);
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.StartInfo.RedirectStandardInput = true;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    proc.EnableRaisingEvents = false;
+                    proc.Start();
+                    proc.WaitForExit();
+                    response = proc.StandardOutput.ReadToEnd();
+                    status = "Success";
+
+                    response = response.Substring(response.IndexOf("\r\n\r\n")).Trim();
+                } else
+                {
+                    status = "Bad Protocol";
+                }
+
+
+                WriteObject(new RemoteAddressInfo(provider, status, response));
+
             }
-            WriteObject(IPAddress.Parse(address));
+
         }
     }
 }
