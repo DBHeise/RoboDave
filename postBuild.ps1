bf41b74bbf7e540745f21ece2baed7953267adba
DBHeise <david@heiseink.com>
2020-01-27 13:26:28 -0500

    updating postBuild script

--- postBuild.ps1
+++ postBuild.ps1
@@ -1,8 +1,10 @@
 [CmdletBinding()]
 param([String]$TargetPath)
 
-$exe = Join-Path $env:LIBDIR 'XmlDoc2CmdletDoc.0.2.13\tools\XmlDoc2CmdletDoc.exe'
+if (Test-Path env:libdir) {
+	$exe = (Get-ChildItem (Join-Path $env:LIBDIR 'XmlDoc2CmdletDoc*\tools\XmlDoc2CmdletDoc.exe') | Sort-Object LastWriteTime | Select-Object -First 1).FullName
 
-if (Test-Path $exe) {	
-	. $exe $TargetPath
+	if (Test-Path $exe) {	
+		. $exe $TargetPath
+	}
 }
